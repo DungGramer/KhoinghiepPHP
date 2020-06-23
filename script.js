990f483a2f21dd566cf660db2ed9a15df9d2c3b9
Nguyễn Công Dũng <dung.dev.gramer@gmail.com>
2020-06-23 16:34:54 +0700

    Update UX / UI Design

--- script.js
+++ script.js
@@ -30,43 +30,74 @@ let product = [
 		image: "xzczx.jpg",
 	},
 ];
-console.log(product["length"]);
+// console.log(product["length"]);
+function render() {
+	for (item of product) {
+		let id = item.id;
+		let name = item.name;
+		let type = item.type;
+		let gender = item.gender;
+		let color = item.color;
+		let size = item.size;
+		let cost = item.cost;
+		let url = item.image;
+		$(".productTable").append(
+			'<tr><th scope="col">' + id + "</th><td>" + name + "</td><td>" + type + "</td><td>" + gender + "</td><td>" + color + "</td><td>" + size + "</td><td>" + cost + "</td><td>" + url + '</td><td><i class="fas fa-user-edit" onclick="updateE(this);"></i></td><td><i data-toggle="modal" data-target="#delItem" class="fas fa-trash" onclick="deleteE(this);"></i></td></tr>'
+		);
+	}
+};
+render();
 
-for (item of product) {
-	let id = item.id;
-	let name = item.name;
-	let type = item.type;
-	let gender = item.gender;
-	let color = item.color;
-	let size = item.size;
-	let cost = item.cost;
-	let url = item.image;
-	$(".productTable").append(
-		'<tr><th scope="col">' +
-			id +
-			"</th><td>" +
-			name +
-			"</td><td>" +
-			type +
-			"</td><td>" +
-			gender +
-			"</td><td>" +
-			color +
-			"</td><td>" +
-			size +
-			"</td><td>" +
-			cost +
-			"</td><td>" +
-			url +
-			'</td><td><i class="fas fa-user-edit" onclick="updateE(this);"></i></td><td><i class="fas fa-trash" onclick="deleteE(this);"></i></td></tr>'
-	);
+function noti(type, message) {
+	let noti = $('[class |="noti"]');
+	if(type == 'success' || type == 'edit') {
+		noti.attr('class', 'noti-'+ type +'').html('<i class="fas fa-check"></i>'+ message +'');
+		setTimeout(function() {
+			noti.html('');
+		}, 5000);
+	} else if ( type == 'warning') {
+		noti.attr('class', 'noti-'+ type +'').html('<i class="far fa-times"></i> '+ message +'');
+		setTimeout(function() {
+			noti.html('');
+		}, 5000);
+	}
 }
 
-$(".btn-primary").click(() => {
+//Click nút thêm mới -> Hiện khung nhập dữ liệu
+$(".addNew").click(() => {
 	$(".addItem").attr("style", "display: block;");
 });
-
-$(".btn-hide").click(() => {
+// Kiểm tra kiểu dữ liệu
+function checking(id, name, type, size, cost) {
+	if (id.length < 2) {
+		noti('warning', 'ID phải lớn hơn 2 ký tự. Vui lòng nhập lại ID');
+		$('input[name = "Product_id"]').focus();
+		return false;
+	}
+	if (name.length < 2) {
+		noti('warning', 'Tên sản phẩm phải lớn hơn 2 ký tự. Vui lòng nhập lại Tên sản phẩm');
+		$('input[name = "Name"]').focus();
+		return false;
+	}
+	if(type.length < 2) {
+		noti('warning', 'Vui lòng nhập loại sản phẩm!');
+		$('input[name = "Type"]').focus();
+		return false;
+	}
+	if(size.length < 2) {
+		noti('warning', 'Vui lòng nhập kích cỡ sản phẩm!');
+		$('input[name = "Size"]').focus();
+		return false;
+	}
+	if( cost <= 0) {
+		noti('warning', 'Vui lòng nhập giá sản phẩm!');
+		$('input[name = "Cost"]').focus();
+		return false;
+	}
+	return true;
+}
+// Nếu click xác nhận thêm sản phẩm
+$(".successAdd").click(() => {
 	let id = $('input[name = "Product_id"]').val();
 	let name = $('input[name = "Name"]').val();
 	let type = $('input[name = "Type"]').val();
@@ -76,49 +107,61 @@ $(".btn-hide").click(() => {
 	let cost = $('input[name = "Cost"]').val();
 	let url = $('input[name = "Image"]').val();
 	let arrID = product.map((x) => x.id);
-	
-	if (arrID.indexOf(id) == -1) {
-		product.push({
-			id: id,
-			name: name,
-			type: type,
-			color: color,
-			size: size,
-			gender: gender,
-			cost: cost,
-			image: url,
-		});
-		$(".productTable").append(
-			'<tr><th scope="col">' +
-				id +
-				"</th><td>" +
-				name +
-				"</td><td>" +
-				type +
-				"</td><td>" +
-				gender +
-				"</td><td>" +
-				color +
-				"</td><td>" +
-				size +
-				"</td><td>" +
-				cost +
-				"</td><td>" +
-				url +
-				'</td><td><i class="fas fa-user-edit" onclick="updateE(this);"></i></td><td><i class="fas fa-trash" onclick="deleteE(this);"></i></td></tr>'
-		);
-	} else {
-		$(".addItem").attr("style", "display: none;");
+	//Kiểm tra kiểm dữ liệu
+	if(checking(id, name, type, size, cost)) {
+		//Kiểm tra trùng ID
+		if (arrID.indexOf(id) == -1) {
+			product.push({
+				id: id,
+				name: name,
+				type: type,
+				color: color,
+				size: size,
+				gender: gender,
+				cost: cost,
+				image: url,
+			});
+			$(".productTable").append(
+				'<tr><th scope="col">' + id + "</th><td>" + name + "</td><td>" + type + "</td><td>" + gender + "</td><td>" + color + "</td><td>" + size + "</td><td>" + cost + "</td><td>" + url + '</td><td><i class="fas fa-user-edit" onclick="updateE(this);"></i></td><td><i data-toggle="modal" data-target="#delItem" class="fas fa-trash" onclick="deleteE(this);"></i></td></tr>'
+			);
+				noti('success', 'Thêm sản phẩm thành công!');
+		 		$('input[name = "Product_id"]').val('');
+				$('input[name = "Name"]').val('');
+				$('input[name = "Type"]').val('');
+				$('select[name = "Gender"]').val('');
+				$('input[name = "Color"]').val('');
+				$('input[name = "Size"]').val('');
+				$('input[name = "Cost"]').val('');
+			 	$('input[name = "Image"]').val('');
+			
+		} else { // Nếu trùng ID
+			noti('warning', 'ID sản phẩm đã được sử dụng. Vui lòng thử một ID khác!');
+			$(".addItem").attr("style", "display: none;");
+		}
+			//Ẩn khung add khi hoàn thành
+			$(".addItem").attr("style", "display: none;");
 	}
-	$(".addItem").attr("style", "display: none;");
+
 });
 
-function deleteE(e) {
-	e.parentElement.parentElement.remove();
-	product.splice(product.indexOf(e.parentElement.parentElement.childNodes[0].innerHTML), 1);
+//Huỷ thêm sản phẩm
+document.querySelector('.cancelAdd').onclick = function() {
+	$(".addItem").attr("style", "display: none;");
 }
 
+//Xoá sản phẩm
+function deleteE(e) {
+	$('.doneDel').click(function() {
+		e.parentElement.parentElement.remove();
+		product.splice(product.indexOf(e.parentElement.parentElement.childNodes[0].innerHTML), 1);
+		$('#delItem').modal('hide');
+
+		noti('success', 'Đã xoá sản phẩm!');
+	});
+}
+//Cập nhật sản phẩm
 function updateE(e) {
+	
 	e.parentElement.parentElement.contentEditable = "true";
 	var thisTR = $(e.parentElement.parentElement);
 	thisTR.focus();
@@ -144,7 +187,7 @@ function updateE(e) {
 		product[index].cost = cost;
 		product[index].url = url;
 		e.parentElement.parentElement.contentEditable = "false";
-	});
 
-	// console.log(e.parentElement);
+		noti('edit', 'Sửa sản phẩm thành công!');
+	});
 }
